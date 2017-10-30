417b84b5c52e936b9aa0aae0a2d61ee4fc7fc89d
廖锦锋 <tac@liaojinfengsAir.lan>
2017-10-31 00:28:50 +0800

    modify configs for myself
    1. disable "gpg" on git and brew
    2. disable display "eol" character on vim
    3. disable "source .bash_profile" when rsync completed

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,12 +7,14 @@ git pull origin master;
 function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
+		--exclude ".gitignore" \
+		--exclude ".gitattributes" \
 		--exclude ".osx" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
-	source ~/.bash_profile;
+#	source ~/.bash_profile;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
